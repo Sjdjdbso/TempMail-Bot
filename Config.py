9e767ef3022cbb7ea104782d77ba78cc477696cd
Leonardo_ <168467152+Sjdjdbso@users.noreply.github.com>
2026-09-12 02:57:40 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
 import os
 
-BOT_TOKEN = os.environ.get("8400958881:AAG9aVEbcBIPPw_5LdK-iSEuMSZKk4kO5Zk")
-API_ID = int(os.environ.get("31645863"))
-API_HASH = os.environ.get("d9d381dd4b7d5f246215d16101d0900d")
+# Baris 3-5 dinonaktifkan dengan tanda pagar (#)
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# API_ID = int(os.environ.get("API_ID"))
+# API_HASH = os.environ.get("API_HASH")
 
-# If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
-
-# BOT_TOKEN = "<your bottoken>"
-# API_ID = "<your api_id>"
-# API_HASH = "<your api_hash>"
+# Baris 9-11 diaktifkan langsung dengan nilai token Anda
+BOT_TOKEN = "8400958881:AAG9aVEbcBIPPw_5LdK-iSEuMSZKk4kO5Zk"
+API_ID = 31645863
+API_HASH = "d9d381dd4b7d5f246215d16101d0900d"
